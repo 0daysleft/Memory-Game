12c5549949c43486873e1ee9871796ba5b393094
odaysleft <vincentarasa254@gmail.com>
2025-04-20 21:12:56 +0300

    created a shuffle function

--- script.js
+++ script.js
@@ -50,3 +50,15 @@ const cardArray =[
      }           
 ]
 
+
+function shuffle(array){
+     for(let i = array.length -1; i >= 0; i--){
+          let j = Math.floor(Math.random() * i + 1);
+
+          [array[i], array[j]] = [array[j], array[i]]
+     }
+
+     return array;
+}
+
+console.log(shuffle(cardArray));
